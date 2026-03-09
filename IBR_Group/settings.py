b85596df636e2f1a02c6efcb122b37ca67ff8ad0
Sphelele <missmakau9@gmail.com>
2026-03-09 16:42:56 +0200

    suport button and removing links on projetcts

--- IBR_Group/settings.py
+++ IBR_Group/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'replace-this-with-a-secure-key'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
