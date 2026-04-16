2f1d6b289abc06cc4bd16b1a15447620de493d65
Sphelele <missmakau9@gmail.com>
2026-04-16 09:44:48 +0200

    debug testing

--- IBR_Group/settings.py
+++ IBR_Group/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'replace-this-with-a-secure-key'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 render_hosts = os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')
